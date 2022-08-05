a4707f190ec0904f6fbeeda5957945fbad45a60b
hwihun.cho <hwihun.cho@navercorp.com>
2022-08-05 23:36:13 +0900

    [20] 상태저장

--- src/components/TodoFooter.tsx
+++ src/components/TodoFooter.tsx
@@ -1,18 +1,32 @@
-import { useContext } from 'react';
-import TodoContext, { TodoConsumer } from '../contexts/todo';
+import { useSelector, useStore } from 'react-redux';
 import styles from '../Todo.module.css'
+
+// type
+import { TodoState } from '../modules/todos'
 
 interface Props {
   readonly onClearAll: () => void
 }
 
 const TodoFooter = ({ onClearAll }: Props) => {
+  const { todos, nextTodoId } = useSelector((state: TodoState) => ({
+    todos: state.todos,
+    nextTodoId: state.nextTodoId,
+  }))
+  const data = {
+    todos,
+    nextTodoId,
+  }
+  const onSave = () => {
+    localStorage.setItem('todo-app-data', JSON.stringify(data))
+  }
   return(
     <div className={styles.footer}>
       <button
         type="button"
         onClick={onClearAll}
       >모두 삭제</button>
+      <button onClick={onSave}>저장</button>
     </div>
   )
 };
