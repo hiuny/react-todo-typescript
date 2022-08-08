813a7238e7ec55c6caa287fff1a67341077c1e4f
hwihun.cho <hwihun.cho@navercorp.com>
2022-08-08 21:53:08 +0900

    [26] dom 요소 접근, 더블클릭시 todo 편집모드 진입

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,7 @@
 import styles from '../Todo.module.css'
 
 import { Todo } from '../App'
+import React, { useEffect, useState } from 'react';
 
 interface Props {
   readonly todo: Todo;
@@ -10,10 +11,68 @@ interface Props {
 
 const TodoItem = ({ todo, onRemove, onToggle }: Props) => {
   const { id, text, done } = todo
+  // 편집 입력 요소 표시여부 상태 선언
+  const [showInput, setShowInput] = useState(false)
+  // 편집 입력 값 상태 선언
+  const [inputText, setInputText] = useState('')
+  // createRef함수를 통한 ref 설정
+  const editInput: React.RefObject<HTMLInputElement> = React.createRef()
+  // 더블 클릭 이벤트 처리 함수
+  const onDoubleClick = () => {
+    console.log('onDoubleClick')
+    setInputText(text)
+    setShowInput(true)
+  }
+  // 변경 이벤트 처리 함수
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('onChange ' + e.target.value)
+    setInputText(e.target.value)
+  }
+  // 키 입력 이벤트 처리 함수
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      console.log('handleKeyPress Enter inputText : ' + inputText)
+      setShowInput(false)
+    }
+  }
+  // 입력 요소 포커스가 사라지면 실행되는 함수
+  const handleBlur = () => {
+    console.log('handleBlur inputText : ' + inputText)
+    setShowInput(false)
+  }
+  // 마운트될 때 편집 입력 요소값 설정
+  useEffect(() => {
+    console.log('useEffect todo = ' + todo)
+    if (todo) {
+      console.log('todo.text = ' + todo.text)
+      setInputText(todo.text)
+    }
+  }, [todo])
+  // 마운트될 때 편집 입력 요소 포커스 처리
+  useEffect(() => {
+    if (editInput.current) {
+      editInput.current.focus()
+    }
+  }, [editInput])
   return (
     <div className={styles.item}>
       <input type="checkbox" checked={done} onChange={() => onToggle(id)}/>
-      <span>{text}</span>
+      {/* 편집 상태일 때 ref 사용한 입력 요소 표시*/}
+      {showInput && (
+        <input
+          value={inputText}
+          onChange={onChange}
+          onKeyPress={handleKeyPress}
+          onBlur={handleBlur}
+          ref={editInput}
+        />
+      )}
+      {/* 편집 상태가 아닐 때 span 요소 표시 */}
+      {!showInput && (
+        <span
+          onDoubleClick={onDoubleClick}
+        >{text}</span>
+      )}
       <button type="button" onClick={() => onRemove(id)}>삭제</button>
     </div>
   )
